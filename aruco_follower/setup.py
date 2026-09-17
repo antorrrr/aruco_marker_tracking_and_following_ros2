282862f3633c8f03ae76d6ef0073591a866498f5
Antor Mondal <140018547+antorrrr@users.noreply.github.com>
2026-09-17 17:42:23 +0600

    Add files via upload

---
+++ aruco_follower/setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+package_name = 'aruco_follower'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Antor',
+    maintainer_email='antor@example.com',
+    description='ArUco marker detection and marker-following for the diffbot AGV',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'aruco_detector = aruco_follower.aruco_detector:main',
+            'marker_follower = aruco_follower.marker_follower:main',
+            'set_target_marker = aruco_follower.set_target_marker:main',
+        ],
+    },
+)
